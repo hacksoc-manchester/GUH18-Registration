bfbcc290dd722f3bf1cb8d1f9738d4cb17ace5f1
Kristijonas Zalys <kzalys@gmail.com>
2019-02-16 11:14:38 +0000

    Update for SHVII

--- app/hackathon_variables.py
+++ app/hackathon_variables.py
@@ -3,35 +3,35 @@ import os
 
 from django.utils import timezone
 
-HACKATHON_NAME = 'GreatUniHack 2018'
+HACKATHON_NAME = 'StudentHack VII'
 # What's the name for the application
-HACKATHON_APPLICATION_NAME = 'GreatUniHack 2018 registration'
+HACKATHON_APPLICATION_NAME = 'StudentHack VII registration'
 # Hackathon timezone
 TIME_ZONE = 'UTC'
 # This description will be used on the html and sharing meta tags
 HACKATHON_DESCRIPTION = '24 hours hacking, 250 attendees and more than 50 hacks.' \
-                        'GreatUniHack 2018 aims at being a full experience and ' \
+                        'StudentHack VII aims at being a full experience and ' \
                         'bringing together a wide range of students from different ' \
                         'fields to showcase their ideas in front of leading companies.'
 # Domain where application is deployed, can be set by env variable
 HACKATHON_DOMAIN = os.environ.get('DOMAIN', 'localhost:8000')
 # Hackathon contact email: where should all hackers contact you. It will also be used as a sender for all emails
-HACKATHON_CONTACT_EMAIL = 'greatunihack@hacksoc.com'
+HACKATHON_CONTACT_EMAIL = 'studenthack@hacksoc.com'
 # Hackathon logo url, will be used on all emails
-HACKATHON_LOGO_URL = 'https://image.ibb.co/hYpwMp/Webp_net_resizeimage_7.png'
+HACKATHON_LOGO_URL = 'https://i.imgur.com/AINL7nY.png'
 
 HACKATHON_OG_IMAGE = 'https://hackcu.org/img/hackcu_ogimage870x442.png'
 # (OPTIONAL) Track visits on your website
 # HACKATHON_GOOGLE_ANALYTICS = 'UA-7777777-2'
 # (OPTIONAL) Hackathon twitter user
-HACKATHON_TWITTER_ACCOUNT = 'GreatUniHack'
+HACKATHON_TWITTER_ACCOUNT = 'StudentHack'
 # (OPTIONAL) Hackathon Facebook page
-HACKATHON_FACEBOOK_PAGE = 'GreatUniHack'
+HACKATHON_FACEBOOK_PAGE = 'StudentHack'
 # (OPTIONAL) Github Repo for this project (so meta)
 HACKATHON_GITHUB_REPO = 'https://github.com/hacksoc-manchester/GUH18-Registration/'
 
 # (OPTIONAL) Applications deadline
-HACKATHON_APP_DEADLINE = timezone.datetime(2018, 11, 12, 11, 00, tzinfo=timezone.pytz.timezone(TIME_ZONE))
+HACKATHON_APP_DEADLINE = timezone.datetime(2019, 3, 15, 11, 00, tzinfo=timezone.pytz.timezone(TIME_ZONE))
 # (OPTIONAL) When to arrive at the hackathon
 # HACKATHON_ARRIVE = 'Registration opens at 3:00 PM and closes at 6:00 PM on Friday October 13th, ' \
 #                    'the opening ceremony will be at 7:00 pm.'
@@ -40,7 +40,7 @@ HACKATHON_APP_DEADLINE = timezone.datetime(2018, 11, 12, 11, 00, tzinfo=timezone
 # HACKATHON_LEAVE = 'Closing ceremony will be held on Sunday October 15th from 3:00 PM to 5:00 PM. ' \
 #                   'However the projects demo fair will be held in the morning from 10:30 AM to 1 PM.'
 # (OPTIONAL) Hackathon live page
-HACKATHON_LIVE_PAGE = 'https://greatunihack.com'
+HACKATHON_LIVE_PAGE = 'https://studenthack.com'
 
 # (OPTIONAL) Regex to automatically match organizers emails and set them as organizers when signing up
 REGEX_HACKATHON_ORGANIZER_EMAIL = '^.*@hacksoc\.com$'
