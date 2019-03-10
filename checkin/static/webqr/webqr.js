0f3cbc7ce05e3a0618d7686d484df36a8c12a109
Sean Parker <sean.parker067@gmail.com>
2019-03-10 13:33:30 +0000

    Change to instascan qr scanner

--- checkin/static/webqr/webqr.js
+++ checkin/static/webqr/webqr.js
@@ -1,17 +1,7 @@
-// QRCODE reader Copyright 2011 Lazar Laszlo
-// http://www.webqr.com
-
 var gCtx = null;
 var gCanvas = null;
-var c = 0;
-var stype = 0;
-var gUM = false;
-var webkit = false;
-var moz = false;
-var v = null;
 
-var vidhtml = '<video id="v" width="100%" autoplay></video>';
-
+var vidhtml = '<video id="hardware-qr-scanner" width:"100%" autoplay></video>';
 
 function initCanvas(w, h) {
     gCanvas = document.getElementById("qr-canvas");
@@ -23,34 +13,6 @@ function initCanvas(w, h) {
     gCtx.clearRect(0, 0, w, h);
 }
 
-
-function captureToCanvas() {
-    if (stype != 1)
-        return;
-    if (gUM) {
-        try {
-            gCtx.drawImage(v, 0, 0);
-            try {
-                qrcode.decode();
-            }
-            catch (e) {
-                console.log(e);
-                setTimeout(captureToCanvas, 500);
-            }
-            ;
-        }
-        catch (e) {
-            console.log(e);
-            setTimeout(captureToCanvas, 500);
-        }
-        ;
-    }
-}
-
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-
 function read(a) {
     location.href = a
 }
@@ -59,29 +21,11 @@ function isCanvasSupported() {
     var elem = document.createElement('canvas');
     return !!(elem.getContext && elem.getContext('2d'));
 }
-function success(stream) {
-    if (webkit)
-        v.src = window.URL.createObjectURL(stream);
-    else if (moz) {
-        v.mozSrcObject = stream;
-        v.play();
-    }
-    else
-        v.src = stream;
-    gUM = true;
-    setTimeout(captureToCanvas, 500);
-}
-
-function error(error) {
-    gUM = false;
-    return;
-}
 
 function load() {
     if (isCanvasSupported() && window.File && window.FileReader) {
         initCanvas(800, 600);
-        qrcode.callback = read;
-        setwebcam();
+        startQRScanner(read);
     }
     else {
         document.getElementById("error").style.display = "inline";
@@ -91,60 +35,31 @@ function load() {
     }
 }
 
-function setwebcam() {
+function couldNotStartCamera() {
+    $.notify({
+      message: 'Could not start webcam!'
+    }, {
+        type: 'danger'
+    });
+  }
 
-    var options = true;
-    if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
-        try {
-            navigator.mediaDevices.enumerateDevices()
-                .then(function (devices) {
-                    devices.forEach(function (device) {
-                        if (device.kind === 'videoinput') {
-                            if (device.label.toLowerCase().search("back") > -1)
-                                options = {'deviceId': {'exact': device.deviceId}, 'facingMode': 'environment'};
-                        }
-                        console.log(device.kind + ": " + device.label + " id = " + device.deviceId);
-                    });
-                    setwebcam2(options);
-                });
-        }
-        catch (e) {
-            console.log(e);
-        }
-    }
-    else {
-        console.log("no navigator.mediaDevices.enumerateDevices");
-        setwebcam2(options);
-    }
-
-}
-
-function setwebcam2(options) {
-    console.log(options);
-    if (stype == 1) {
-        setTimeout(captureToCanvas, 500);
-        return;
-    }
-    var n = navigator;
-    document.getElementById("outdiv").innerHTML = vidhtml;
-    v = document.getElementById("v");
-
-
-    if (n.getUserMedia) {
-        webkit = true;
-        n.getUserMedia({video: options, audio: false}, success, error);
-    }
-    else if (n.webkitGetUserMedia) {
-        webkit = true;
-        n.webkitGetUserMedia({video: options, audio: false}, success, error);
-    }
-    else if (n.mozGetUserMedia) {
-        moz = true;
-        n.mozGetUserMedia({video: options, audio: false}, success, error);
-    }
-
-
-    stype = 1;
-    setTimeout(captureToCanvas, 500);
-}
+function startQRScanner(callback) {
+    closeScanner();
+    scanner = new Instascan.Scanner({ video: document.getElementById('hardware-qr-scanner'), mirror: false });
+    scanner.addListener('scan', function (content) {
+      callback(content);
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+      if (cameras.length > 0) {
+        maxCameras = cameras.length;
+        camera = cameras[camera_option];
+        scanner.start(camera);
+      } else {
+        couldNotStartCamera();
+      }
+    }).catch(function (e) {
+      couldNotStartCamera();
+      console.error(e);
+    });
+  }
 
